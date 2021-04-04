e95ff284217ae2b44cc74f567cee3dfcb6589a59
Nija <nija.perry21@gmail.com>
2021-04-04 11:40:49 -0400

    commit tree 273bccdec25edca5d11eb46c1f2c91f88b5fee83

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function createEmployeeRecord(firstName, lastName, title, payPerHour) {
+
+
+}
+
+function createEmployeeRecords() {
+
+}
+
+function createTimeInEvent() {
+
+}
+
+function hoursWorkedOnDate() {
+
+}
+
+function wagesEarnedOnDate() {
+
+}
+
+function allWagesFor() {
+
+}
+
+function findEmployeeByFirstName() {
+
+}
+
+function calculatePayroll() {
+
+}
